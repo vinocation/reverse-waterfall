b21cd8d58a86b4e9615bc6809c27b098192416dd
vinocation <vinocation.x@gmail.com>
2021-10-28 22:26:00 +0800

    setup bolg

---
+++ src/components/layout.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { Link, useStaticQuery, graphql } from "gatsby";
+import {
+  container,
+  siteTitle,
+  heading,
+  navLinks,
+  navLinkItem,
+  navLinkText,
+} from "./layout.module.css";
+
+const Layout = ({ pageTitle, children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
+  return (
+    <div className={container}>
+      <title>
+        {pageTitle} | {data.site.siteMetadata.title}
+      </title>
+      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
+      <nav>
+        <ul className={navLinks}>
+          <li className={navLinkItem}>
+            <Link to="/" className={navLinkText}>
+              Home
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="/blog" className={navLinkText}>
+              Blog
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="/about" className={navLinkText}>
+              About
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <h1 className={heading}>{pageTitle}</h1>
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/layout";
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p> Hi this is me !</p>
+    </Layout>
+  );
+};
+
+export default AboutPage;
